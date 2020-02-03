ab1c1948969a9b14d9b23905aac2950b0111f226
julita_koss@tlen.pl <julitakoss@github.com>
2020-02-03 11:36:01 +0100

    commit projektu

---
+++ main.js
@@ -0,0 +1,21 @@
+function funkcja (y, z){
+    let x;
+
+    x = y+z;
+    console.log('Wynik dodawania wynosi '+x);   
+    if(g<0){
+        console.log('Wynik jest ujemny');
+    }
+
+    x = y-z;
+    console.log('Wynik odejmowania wynosi '+x);
+    if(g<0){
+        console.log('Wynik jest ujemny');
+    }
+    
+    x = y*z;
+    console.log('Wynik mnożenia wynosi ' +x);
+    if(g<0){
+        console.log('Wynik jest ujemny');
+    }
+}
