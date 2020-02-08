09845719351b134ef03bce4063d322f040572a2d
julita_koss@tlen.pl <julitakoss@github.com>
2020-02-08 12:03:14 +0100

    dodanie brakujacych elementow

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
+funkcja(10,-10);
 function funkcja (y, z){
     let x;
 
     x = y+z;
     console.log('Wynik dodawania wynosi '+x);   
-    if(g<0){
+    if(x<0){
         console.log('Wynik jest ujemny');
     }
 
     x = y-z;
     console.log('Wynik odejmowania wynosi '+x);
-    if(g<0){
+    if(x<0){
         console.log('Wynik jest ujemny');
     }
     
     x = y*z;
     console.log('Wynik mnożenia wynosi ' +x);
-    if(g<0){
+    if(x<0){
         console.log('Wynik jest ujemny');
     }
 }
